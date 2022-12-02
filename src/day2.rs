f4b14e77fde3ac0a24aee8eb6154179f8a936514
Victor Sonck <victor.sonck@gmail.com>
2022-12-02 12:09:02 +0100

    This was way harder than it should have been

--- src/day2.rs
+++ src/day2.rs
@@ -1,18 +1,66 @@
-use std::{fs::File, io::BufReader};
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
 
 pub fn day2_1(filepath: &str) -> Result<i32, std::io::Error> {
     let file = File::open(filepath)?;
     let reader = BufReader::new(file);
 
-    Ok()
+    let lines: Vec<String> = reader.lines().map(|s| s.expect("Could not read line!")).collect();
+    // println!("{:?}", lines);
+    let array: Vec<Vec<i32>> = lines.iter().map(|line| line.replace(" ", "").chars().map(
+        |x| match x { 
+            'A' | 'X' => 1, 
+            'B' | 'Y' => 2, 
+            'C' | 'Z' => 3,
+            _ => -1
+        }).collect()).collect();
+    // println!("{:?}", array);
+
+    let scores: Vec<i32> = array.iter().map(
+        // The score of the game is the value of whatever you played
+        |single_game| single_game[1] 
+        // + 6 if you won, 0 if you didn't
+        + 6 * (single_game[1] - 1 == (single_game[0] % 3)) as i32
+        // + 3 if it was a tie. This line and previous line will never fire at the same time
+        + 3 * (single_game[0] == single_game[1]) as i32
+    ).collect();
+
+    // println!("{:?}", scores);
+
+    Ok(scores.iter().sum())
 }
+
+
 
 pub fn day2_2(filepath: &str) -> Result<i32, std::io::Error> {
     let file = File::open(filepath)?;
     let reader = BufReader::new(file);
 
-    Ok()
+    let lines: Vec<String> = reader.lines().map(|s| s.expect("Could not read line!")).collect();
+    // println!("{:?}", lines);
+    let array: Vec<Vec<i32>> = lines.iter().map(|line| line.replace(" ", "").chars().map(
+        |x| match x { 
+            'A' | 'X' => 0, 
+            'B' | 'Y' => 1, 
+            'C' | 'Z' => 2,
+            _ => -1
+        }).collect()).collect();
+    // println!("{:?}", array);
+
+    let scores: Vec<i32> = array.iter().map(
+        // The score of the game is the value of whatever you played
+        |single_game| match single_game {
+            _ if single_game[1] == 0 => ((3 + single_game[0] - 1) % 3) + 1,  // lose
+            _ if single_game[1] == 1 => single_game[0] + 4,  // draw
+            _ if single_game[1] == 2 => 6 + (((single_game[0] + 1) % 3) + 1),  // win
+            _ => 0
+        }
+    ).collect();
+
+    // println!("{:?}", scores);
+
+    Ok(scores.iter().sum())
 }
 
 #[cfg(test)]
@@ -22,11 +70,16 @@ mod tests {
 
     #[test]
     fn test_day2_1() {
-        assert_eq!(day2_1("data/test1_1.txt").unwrap(), 24000);
+        assert_eq!(day2_1("data/test2_1.txt").unwrap(), 15);
+    }
+
+    #[test]
+    fn test_day2_1_bis() {
+        assert_eq!(day2_1("data/test2_2.txt").unwrap(), 15);
     }
 
     #[test]
     fn test_day2_2() {
-        assert_eq!(day2_2("data/test1_1.txt").unwrap(), 45000);
+        assert_eq!(day2_2("data/test2_2.txt").unwrap(), 12);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,22 @@ mod day1;
 mod day2;
 
 fn main() -> io::Result<()> {
-    let total = day1::day1_1("data/input1_1.txt");
-    println!("{}", total.unwrap());
+    let day = 2;
 
-    let total_2 = day1::day1_2("data/input1_1.txt");
-    println!("{}", total_2.unwrap());
+    if day == 1 {
+        let total = day1::day1_1("data/input1_1.txt");
+        println!("Final total: {}", total.unwrap());
+
+        let total_2 = day1::day1_2("data/input1_1.txt");
+        println!("{}", total_2.unwrap());
+    }
+
+    if day == 2 {
+        let total = day2::day2_1("data/input2_1.txt");
+        println!("{}", total.unwrap());
+
+        let total = day2::day2_2("data/input2_1.txt");
+        println!("{}", total.unwrap());
+    }
     Ok(())
 }
