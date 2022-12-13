01805c290330a06fe3abdc7f7c5d73f4f4d02f2c
Victor Sonck <victor.sonck@gmail.com>
2022-12-13 16:52:35 +0100

    Python is so much easier holy shit

--- src/day13.py
+++ src/day13.py
@@ -1,9 +1,6 @@
 import functools
 
-def compare(x, y):
-    packet1 = x
-    packet2 = y
-    print(f"Comparing {packet1} to {packet2}")
+def compare(packet1, packet2):
     index = 0
     while True:
         p1 = packet1[index] if index < len(packet1) else None
@@ -40,9 +37,24 @@ def compare(x, y):
         
         # Only here if p1 == p2
         index += 1
+
+def bubbleSort(arr):
+    n = len(arr)
+ 
+    # Traverse through all array elements
+    for i in range(n):
+ 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+ 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if not compare(arr[j], arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
         
 
-with open("data/test13_1.txt", "r") as input_file:
+with open("data/input13_1.txt", "r") as input_file:
     # Part 1
     chunk_list = input_file.read().split("\n\n")
     answers = []
@@ -66,11 +78,11 @@ with open("data/test13_1.txt", "r") as input_file:
         new_chunk_list += [eval(c) for c in chunk.split("\n")]
     new_chunk_list.append([[2]])
     new_chunk_list.append([[6]])
-    part_2 = sorted(new_chunk_list, key=functools.cmp_to_key(compare))
-    print(part_2)
-    divider_1 = part_2.index([[2]])
+    bubbleSort(new_chunk_list)
+    print(new_chunk_list)
+    divider_1 = new_chunk_list.index([[2]]) + 1
     print(divider_1)
-    divider_2 = part_2.index([[6]])
+    divider_2 = new_chunk_list.index([[6]]) + 1
     print(divider_2)
     
     print(divider_1 * divider_2)
