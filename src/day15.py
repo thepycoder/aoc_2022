5ebb1a31ec389f1a59c95599fb163edae8b52a4c
Victor Sonck <victor.sonck@gmail.com>
2022-12-16 15:57:19 +0100

    I MAKE FASTUR

--- src/day15.py
+++ src/day15.py
@@ -1,6 +1,8 @@
 import math
 import re
 
+from tqdm import tqdm
+
 
 class Coord:
     def __init__(self, x, y):
@@ -24,9 +26,6 @@ def read_input(filename):
     regex = r"Sensor at x=(.*), y=(.*): closest beacon is at x=(.*), y=(.*)"
     sensors = []
     beacons = []
-    largest_distance_between_sensor_and_beacon = 0
-    most_left = 0
-    most_right = 0
     with open(filename, 'r') as f:
         for line in f.readlines():
             groups = re.findall(regex, line, flags=re.MULTILINE)[0]
@@ -35,16 +34,7 @@ def read_input(filename):
             sensors.append(sensor)
             beacons.append(beacon)
 
-            if sensor - beacon > largest_distance_between_sensor_and_beacon:
-                largest_distance_between_sensor_and_beacon = sensor - beacon
-            
-            for c in (sensor, beacon):
-                if c.x < most_left:
-                    most_left = c.x
-                if c.x > most_right:
-                    most_right = c.x
-
-    return sensors, beacons, largest_distance_between_sensor_and_beacon, most_left, most_right
+    return sensors, beacons
 
 
 def combine_ranges(ranges):
@@ -54,12 +44,12 @@ def combine_ranges(ranges):
         flag = True
         for erange in combined_ranges:
             # if overlapping left
-            if irange[0] < erange[0] and erange[0] <= irange[1] <= erange[1]:
+            if irange[0] < erange[0] and erange[0] - 1 <= irange[1] <= erange[1]:
                 erange[0] = irange[0]
                 flag = False
                 continue
             # if overlapping right
-            if irange[1] >= erange[1] and erange[0] <= irange[0] <= erange[1]:
+            if irange[1] >= erange[1] and erange[0] <= irange[0] <= erange[1] + 1:
                 erange[1] = irange[1]
                 flag = False
                 continue
@@ -82,30 +72,43 @@ def combine_ranges(ranges):
     return combined_ranges
 
 
-def day15(filename, rownr):
+@profile
+def day15_1(sensors, beacons, rownr):
     row = []
-    sensors, beacons, largest_distance_between_sensor_and_beacon, most_left, most_right = read_input(filename)
     for sensor, beacon in zip(sensors, beacons):
         sensor_range = sensor - beacon
         if sensor.y + sensor_range >= rownr >= sensor.y - sensor_range:
             x_distance_sensor_left_intersection = sensor_range - abs(sensor.y - rownr)
-            left_intersection = Coord(sensor.x - x_distance_sensor_left_intersection, rownr)
-            right_intersection = Coord(sensor.x + x_distance_sensor_left_intersection, rownr)
-            row.append([left_intersection.x, right_intersection.x])
+            row.append([sensor.x - x_distance_sensor_left_intersection, sensor.x + x_distance_sensor_left_intersection])
 
     combined_ranges = combine_ranges(row)
-    print(combined_ranges)
+    # print(combined_ranges)
     total = 0
     for r in combined_ranges:
         total += r[1] - r[0]
     # print(len(row), sorted([c.x for c in row]))
-    print(total)
+    # print(total)
+
+    return total, combined_ranges
+
+
+def day15_2(sensors, beacons, max_size):
+    for rownr in tqdm(range(max_size)):
+        _, combined_range_row = day15_1(sensors, beacons, rownr)
+        if len(combined_range_row) > 1:
+            x = combined_range_row[0][1] + 1
+            return x * 4000000 + rownr
+
 
 if __name__ == '__main__':
+    test_sensors, test_beacons = read_input("data/test15_1.txt")
+    sensors, beacons = read_input("data/input15_1.txt")
     # pairs = [[1, 20], [15, 20], [25, 30]]
     # minimum = 0
     # maximum = 30
     # missing_ranges = combine_ranges(pairs)
     # print(missing_ranges)
-    day15("data/test15_1.txt", 10)
-    day15("data/input15_1.txt", 2000000)
+    print(day15_1(test_sensors, test_beacons, 10))
+    print(day15_1(sensors, beacons, 2000000))
+    print(day15_2(test_sensors, test_beacons, 20))
+    print(day15_2(sensors, beacons, 4000000))
